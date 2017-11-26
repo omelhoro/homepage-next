2ae6defc288557b247da3001cf84d506404f0288
omelhoro <igor.fischer154@gmail.com>
2017-11-26 03:41:50 +0100

    Add piwik tag

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import 'isomorphic-fetch'
 import Head from 'next/head';
 
+const trackingCode = `
+var _paq = _paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//tracking.igor-fischer.rocks/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', '1']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+`;
+
 export default class extends React.Component {
   static async getInitialProps ({res}) {
     try {
@@ -26,19 +40,7 @@ export default class extends React.Component {
         <Head>
           <link rel="stylesheet" type="text/css" href="/static/main.css" />
           <link href="https://fonts.googleapis.com/css?family=Ubuntu+Condensed" rel="stylesheet" />
-          <script type="text/javascript">
-            var _paq = _paq || [];
-            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-              var u="//tracking.igor-fischer.rocks/";
-              _paq.push(['setTrackerUrl', u+'piwik.php']);
-              _paq.push(['setSiteId', '1']);
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-            })();
-          </script>
+          <script type="text/javascript" dangerouslySetInnerHTML={{__html: trackingCode}} />
         </Head>
         <div dangerouslySetInnerHTML={{__html: this.props.html}} />
       </div>
