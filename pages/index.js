dc3e749b9e96358686285ed290a3e61ef7168f87
omelhoro <igor.fischer154@gmail.com>
2017-11-26 03:31:23 +0100

    Add piwik tag

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,19 @@ export default class extends React.Component {
         <Head>
           <link rel="stylesheet" type="text/css" href="/static/main.css" />
           <link href="https://fonts.googleapis.com/css?family=Ubuntu+Condensed" rel="stylesheet" />
+          <script type="text/javascript">
+            var _paq = _paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//tracking.igor-fischer.rocks/";
+              _paq.push(['setTrackerUrl', u+'piwik.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+            })();
+          </script>
         </Head>
         <div dangerouslySetInnerHTML={{__html: this.props.html}} />
       </div>
